4ed5410e44bb5bf2cf0e38f0a3d21fa02f8b6479
Khalah Jones-Golden <khasan222@gmail.com>
2015-08-26 12:27:49 -0400

    [Build]

--- build/jubilee.js
+++ build/jubilee.js
@@ -10056,30 +10056,30 @@ define('src/modules/component/events',['require','d3','src/modules/valuator','sr
   var targetIndex = require("src/modules/helpers/target_index");
 
   return function events() {
-    var accessor = function (d) { return d.x; };
+    // Private variables
     var listeners = {};
 
     function component(selection) {
-      selection.each(function (data, index) {
+      selection.each(function () {
         var element = d3.select(this);
 
-        d3.entries(listeners).forEach(function (e, i) {
-
-          // Stop listening for event types that have an empty listeners
-          // array or are set to null
+        d3.entries(listeners).forEach(function (e) {
+          // Stop listening for event types that have
+          // an empty listeners array or that is set to null
           if (!e.value || !e.value.length) {
             return element.on(e.key, null);
           }
 
-          element.on(e.key, function (d, i) {
+          element.on(e.key, function () {
             d3.event.stopPropagation(); // => event.stopPropagation()
 
             e.value.forEach(function (listener) {
               // References the data point to calculate the correct index value
+              var svg = d3.event.target.farthestViewportElement;
               var target = d3.select(d3.event.target);
-              var parent = d3.select(d3.event.target.farthestViewportElement);
+              var parent = !svg ? d3.select(d3.event.target) : d3.select(svg);
               var datum = target.datum();
-              var index = targetIndex(parent, target);
+              var index = targetIndex(parent, target) || 0;
 
               listener.call(this, d3.event, datum, index);
             });
@@ -10089,12 +10089,6 @@ define('src/modules/component/events',['require','d3','src/modules/valuator','sr
     }
 
     // Public API
-    component.accessor = function (_) {
-      if (!arguments.length) { return accessor; }
-      accessor = valuator(_);
-      return component;
-    };
-
     component.listeners = function (_) {
       if (!arguments.length) { return listeners; }
       listeners = _;
